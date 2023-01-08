1a8f04e923b6d6b29993b6dec9d95cec85c61ddd
chandbud5 <chandbud5@gmail.com>
2023-01-08 19:26:57 +0530

    Added env variables

--- src/apiHelper.js
+++ src/apiHelper.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "http://localhost:4000/" 
+    baseURL: process.env.REACT_APP_API_HOST
 });
 
 async function axiosGetRequest(path){
+    console.log(process.env.REACT_APP_API_HOST);
     const response = await client.get(path);
     return response
 }
 
 async function axiosPostRequest(path, body){
+    console.log(process.env.REACT_APP_API_HOST);
     const data = await client.post(path, body,{
         headers: { 
             "Content-Type": "application/x-www-form-urlencoded"
