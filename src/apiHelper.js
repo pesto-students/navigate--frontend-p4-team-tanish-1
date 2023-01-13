3879967c915e5414dc6517b9ee38a0869ca95367
chandbud5 <chandbud5@gmail.com>
2023-01-13 18:31:37 +0530

    host URL from env

--- src/apiHelper.js
+++ src/apiHelper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-    baseURL: "http://localhost:4000/" 
+    baseURL: process.env.REACT_APP_API_HOST,
 });
 
 let accessToken = localStorage.getItem("access");
